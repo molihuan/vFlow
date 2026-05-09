e516e2eb215be3154aac641fd119c7986f91dc22
ChaoMixian <chaomingxun@outlook.com>
2026-05-09 18:14:26 +0800

    chore(Version): 更新版本号

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.chaomixian.vflow"
         minSdk = 29
         targetSdk = 36
-        versionCode = 47
-        versionName = "1.5.0"
+        versionCode = 48
+        versionName = "1.5.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
